a15bae19d5f48991bc09c60f13845c89bf043758
nurbqq <nurbakhyt.sembayev@gmail.com>
2023-06-07 00:27:10 +0500

    add ticket controller, model and pages not finished

---
+++ src/main/java/kz/kasky/cinemaroom/controllers/TicketController.java
@@ -0,0 +1,61 @@
+package kz.kasky.cinemaroom.controllers;
+
+
+import jakarta.validation.Valid;
+import kz.kasky.cinemaroom.models.dto.TicketDto;
+import kz.kasky.cinemaroom.services.TicketService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketController {
+
+    private final TicketService ticketService;
+
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+
+    @GetMapping
+    public String getAllTickets(Model model) {
+        List<TicketDto> ticketDtoList = ticketService.getAllTickets();
+
+        model.addAttribute("tickets", ticketDtoList);
+
+        return "ticket_page/tickets-list";
+    }
+
+
+    @GetMapping("/{movieId}/new")
+    public String createTicketForm(@PathVariable("movieId") Integer clubId, Model model) {
+        TicketDto ticketDto = new TicketDto();
+
+        model.addAttribute("clubId", clubId);
+        model.addAttribute("ticket", ticketDto);
+
+        return "ticket_page/tickets-create";
+    }
+
+
+    @PostMapping("/new")
+    public String saveTicket(@Valid @ModelAttribute("ticket") TicketDto ticketDto,
+                             BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("ticket", ticketDto);
+            return "ticket_page/ticket_create";
+        }
+
+        ticketService.saveTicket(ticketDto);
+
+
+        return "redirect:/tickets";
+    }
+}
---
+++ src/main/java/kz/kasky/cinemaroom/models/entities/Ticket.java
@@ -0,0 +1,33 @@
+package kz.kasky.cinemaroom.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    private Integer orderId;
+
+    private Integer theaterId;
+
+    private Integer spot;
+
+    private Boolean isPurchased;
+
+
+}
