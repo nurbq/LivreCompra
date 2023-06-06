bdbfb978c6989f184a2f0472f0a4a1e1ded7cc7c
nurbqq <nurbakhyt.sembayev@gmail.com>
2023-06-07 00:29:02 +0500

    add HomeController

---
+++ src/main/java/kz/kasky/cinemaroom/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package kz.kasky.cinemaroom.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping
+    public String homePage() {
+        return "main";
+    }
+
+}
